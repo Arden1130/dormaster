777926a1f68645bc7ef455ede619c3cfa7d2867e
Arden <wwwlujialincom>
2023-02-25 20:36:42 +0800

    提交建立好的项目框架

---
+++ src/main/java/com/arden/dormaster/DormasterApplication.java
@@ -0,0 +1,13 @@
+package com.arden.dormaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DormasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arden/dormaster/DormasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arden.dormaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
