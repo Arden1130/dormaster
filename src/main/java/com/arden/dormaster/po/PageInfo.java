b5eaf42f31950d717fb71480ca5553d4e79552c0
Arden <1328635912@qq.com>
2023-03-16 09:47:07 +0800

    写了一下登录功能服务层和pageinfo的javabean

---
+++ src/main/java/com/arden/dormaster/po/PageInfo.java
@@ -0,0 +1,8 @@
+package com.arden.dormaster.po;
+
+public class PageInfo {
+    private Integer pageIndex=1; //页码
+    private Integer pageSize=3; //显示条数
+    private Integer totalCount=0;  //总条数
+    private Integer pageTotalCount=0; //总页数
+}
--- src/main/java/com/arden/dormaster/service/AdminService.java
+++ src/main/java/com/arden/dormaster/service/AdminService.java
@@ -1,4 +1,28 @@
 package com.arden.dormaster.service;
 
+import com.arden.dormaster.po.Admin;
+
+import java.util.List;
+
 public interface AdminService {
+    //通过账号和密码查询用户
+    public Admin findAdmin(Admin admin);
+
+    //找到所有数据
+    public List<Admin> getAll();
+
+     //分页查询
+    public PageInfo<Admin> findPageInfo(String a_username,String a_describe,Integer a_id,Integer pageIndex,Integer pageSize);
+
+    //添加管理员信息
+    public int addAdmin(Admin admin);
+
+    //删除管理员信息
+    public int deleteAdmin(Integer a_id);
+
+    //修改管理员信息
+    public int updateAdmin(Admin admin);
+
+    public Admin findAdminById(Integer a_id);
+
 }
