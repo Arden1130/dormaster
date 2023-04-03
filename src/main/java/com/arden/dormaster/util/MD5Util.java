e2482417401fd5edf1f810e00d50b1a83351309b
Arden <1328635912@qq.com>
2023-04-04 01:12:02 +0800

    写了点MD5加密

--- src/main/java/com/arden/dormaster/util/MD5Util.java
+++ src/main/java/com/arden/dormaster/util/MD5Util.java
@@ -1,6 +1,6 @@
 package com.arden.dormaster.util;
 
-public class MD5Util {//MD5加密代码
+public class MD5Util {//MD5加密代码函数
 
     private static String byteArrayToHexString(){
         StringBuffer resultSb=new StringBuffer();
