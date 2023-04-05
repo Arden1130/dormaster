54327f021ea2e1f2628bf02ed87ad0e0c27c3414
Arden <1328635912@qq.com>
2023-04-06 01:28:39 +0800

    MD5

--- src/main/java/com/arden/dormaster/util/MD5Util.java
+++ src/main/java/com/arden/dormaster/util/MD5Util.java
@@ -1,6 +1,6 @@
 package com.arden.dormaster.util;
 
-public class MD5Util {//MD5加密函数
+public class MD5Util {//MD5加密函数啊啊啊啊啊啊
 
     private static String byteArrayToHexString(){
         StringBuffer resultSb=new StringBuffer();
